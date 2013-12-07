50463f3b8c27ebecb5b5cffe17e153230620cb51
nana77sun <nana77_sun@sina.com>
2013-12-07 13:52:08 +0800

    引用smartcore的connection

--- api/shortmail.js
+++ api/shortmail.js
@@ -1,24 +1,24 @@
 var shortmail = require('../controllers/ctrl_shortmail')
-  , amqp = lib.core.amqp
-  , json = lib.core.json;
+  , amqp = smart.framework.amqp
+  , response = smart.framework.response;
 
 /**
  * 发送私有信
  */
-exports.sendPrivateMessage = function(req_, res_) {
+exports.sendPrivateMessage = function(req, res) {
 
   // TODO: 更改变量名 body._id -> to
   var mail = {
-      to: req_.body._id
-    , message: req_.body.message
-    , by: req_.session.user._id
+      to: req.body._id
+    , message: req.body.message
+    , by: req.session.user._id
     };
 
   // 将消息保存到数据库中
   shortmail.create(mail, function(err, result){
 
     if (err) {
-      return res_.send(json.errorSchema(err.code, err.message));
+      return response.send(res, err);
     } else {
 
       // 保存成功发送消息
@@ -32,7 +32,7 @@ exports.sendPrivateMessage = function(req_, res_) {
         });
       console.log(mail.message);
 
-      return res_.send(json.dataSchema(result));
+      return response.send(res, err, result);
     }
   });
 };
@@ -40,97 +40,97 @@ exports.sendPrivateMessage = function(req_, res_) {
 /**
  * 获取未读的私信
  */
-exports.getUnreadList = function(req_, res_) {
+exports.getUnreadList = function(req, res) {
 
-  var uid = req_.session.user._id;
+  var uid = req.session.user._id;
 
   shortmail.unread(uid, function(err, result){
     if (err) {
-      return res_.send(json.errorSchema(err.code, err.message));
+      return response.send(res, err);
     } else {
-      return res_.send(json.dataSchema(result));
+      return response.send(res, err, result);
     }
   });
 };
 
-exports.getMailUser = function(req_, res_) {
+exports.getMailUser = function(req, res) {
 
-  var _id = req_.session.user._id;
+  var _id = req.session.user._id;
   shortmail.getMailUser(_id, function(err, result){
     if (err) {
-      return res_.send(json.errorSchema(err.code, err.message));
+      return response.send(res, err);
     } else {
-      return res_.send(json.dataSchema({items: result}));
+      return response.send(res, err, {items: result});
     }
   });
 };
 
-exports.getContacts = function(req_, res_) {
-  var uid = req_.session.user._id
-    , firstLetter  = req_.query.firstLetter
-    , start = req_.query.start
-    , limit = req_.query.count;
+exports.getContacts = function(req, res) {
+  var uid = req.session.user._id
+    , firstLetter  = req.query.firstLetter
+    , start = req.query.start
+    , limit = req.query.count;
 
   shortmail.getContacts(uid, firstLetter, start, limit, function(err, result){
     if (err) {
-      res_.send({msg: 'bad request exception'});
+      return response.send(res, err);
     } else {
-      res_.send(json.dataSchema({items: result}));
+      return response.send(res, err, {items: result});
     }
   });
 
 };
 
-exports.getMailList = function(req_, res_) {
+exports.getMailList = function(req, res) {
 
-  var contact = req_.query.contact
-    , date = req_.query.date
-    , count = req_.query.date;
+  var contact = req.query.contact
+    , date = req.query.date
+    , count = req.query.date;
 
   shortmail.getMailList(contact, date, count, function(err, result){
     if (err) {
-      return res_.send(json.errorSchema(err.code, err.message));
+      return response.send(res, err);
     } else {
-      return res_.send(json.dataSchema({"items": result}));
+      return response.send(res, err, {items: result});
     }
   });
 
 
-  // var _id = req_.session.user._id;
-  // var uid = req_.query.uid
-  //   , type = req_.query.type
-  //   , date = req_.query.date;
+  // var _id = req.session.user._id;
+  // var uid = req.query.uid
+  //   , type = req.query.type
+  //   , date = req.query.date;
 
   // if (type === "earlier") {
   //   shortmail.getEarlierMails(_id, uid, date, function(err, result){
   //     if (err) {
-  //       return res_.send(json.errorSchema(err.code, err.message));
+  //       return res.send(json.errorSchema(err.code, err.message));
   //     } else {
-  //       return res_.send(json.dataSchema({"items": result}));
+  //       return res.send(json.dataSchema({"items": result}));
   //     }
   //   });
 
   // } else {
   //   shortmail.getMailList(_id, uid, function(err, result){
   //     if (err) {
-  //       return res_.send(json.errorSchema(err.code, err.message));
+  //       return res.send(json.errorSchema(err.code, err.message));
   //     } else {
-  //       return res_.send(json.dataSchema({"items": result}));
+  //       return res.send(json.dataSchema({"items": result}));
   //     }
   //   });
   // }
 };
 
-exports.getEarlierMails = function(req_, res_){
-  var _id = req_.session.user._id
-    , _uid = req_.query.uid
-    , _date = req_.query.date;
+exports.getEarlierMails = function(req, res){
+  var _id = req.session.user._id
+    , _uid = req.query.uid
+    , _date = req.query.date;
 
   shortmail.getEarlierMails(_id, _uid, _date, function(err, result){
     if (err) {
-      res_.send({msg: 'bad request exception'});
+      return response.send(res, err);
     } else {
-      res_.send(json.dataSchema({items: result}));
+      return response.send(res, err, {items: result});
     }
   });
 };
