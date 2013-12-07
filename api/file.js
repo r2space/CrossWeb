b0ebe5f8a358277d3c5ba22f3cffbc192a1437b0
nana77sun <nana77_sun@sina.com>
2013-12-07 16:06:21 +0800

    对应图片上传下载

---
+++ api/file.js
@@ -0,0 +1,39 @@
+/**
+ * API File
+ * Copyright (c) 2012 Author Name dd_dai
+ */
+
+"use strict";
+
+var ctrlFile     = require("../controllers/ctrl_file")
+  , context  = smart.framework.context
+  , response = smart.framework.response;
+
+
+exports.save = function(req, res){
+
+  var handler = new context().bind(req, res);
+
+  ctrlFile.save(handler, function(err, result) {
+    if (err) {
+      return response.send(res, err);
+    } else {
+      return response.send(res, err, {items: result});
+    }
+  });
+};
+
+exports.image = function(req, res) {
+
+  var handler = new context().bind(req, res);
+
+  ctrlFile.get(handler, function(err, result){
+
+    if (err) {
+      return response.send(res, err);
+    }
+
+    return response.sendFile(res, err, result);
+  });
+
+};
